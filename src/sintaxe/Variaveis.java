34c463bcabef780996cb7e822277a02c78457496
Giovana Assis <giovanaa@ciandt.com>
2022-05-20 14:58:22 -0300

    basic sintaxe

---
+++ src/sintaxe/Variaveis.java
@@ -0,0 +1,13 @@
+package src.sintaxe;
+
+public class Variaveis {
+
+  public static void main(String[] args) {
+    System.out.println("Olá novo teste");
+
+    int idade;
+    idade = 37;
+
+    System.out.println("A idade é " + idade);
+  }
+}
