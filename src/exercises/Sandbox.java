f118910079fb5b4b5a27711bce67e18b8fd4791b
Giovana Assis <giovana.sant@hotmail.com>
2022-06-04 23:19:45 -0300

    exercises

---
+++ src/exercises/Sandbox.java
@@ -0,0 +1,7 @@
+public class Sandbox {
+    public static void main(String[] args) {
+        for (String line = "#"; line.length() < 8; line += "#") {
+            System.out.println(line);
+        }
+    }
+}
